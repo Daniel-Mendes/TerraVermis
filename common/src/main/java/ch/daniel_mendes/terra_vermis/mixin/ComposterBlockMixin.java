e1b9788084ed8a0f897b487efce8ac99dd29aab0
Daniel Mendes <contact@daniel-mendes.ch>
2025-05-16 19:00:08 +0200

    refactor: renamed earthwormFarm to earthwormDirt in ComposterBlockMixin

--- common/src/main/java/ch/daniel_mendes/terra_vermis/mixin/ComposterBlockMixin.java
+++ common/src/main/java/ch/daniel_mendes/terra_vermis/mixin/ComposterBlockMixin.java
@@ -19,14 +19,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ComposterBlockMixin {
 
     @Unique
-    private static final int EARTHWORM_DROP_CHANCE = 5;
+    private static final int EARTHWORM_DIRT_DROP_CHANCE = 5;
 
     @Inject(method = "extractProduce", at = @At("HEAD"))
-    private static void dropEarthwormFarmChance(Entity entity, BlockState state, Level level, BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
-        if (!level.isClientSide && level.random.nextInt(100) < EARTHWORM_DROP_CHANCE) {
+    private static void dropEarthwormDirtChance(Entity entity, BlockState state, Level level, BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
+        if (!level.isClientSide && level.random.nextInt(100) < EARTHWORM_DIRT_DROP_CHANCE) {
             Vec3 vec3 = Vec3.atLowerCornerWithOffset(pos, 0.5, 1.01, 0.5).offsetRandom(level.random, 0.7F);
-            ItemStack earthwormFarm = new ItemStack(BlocksRegistry.WORMY_DIRT.get());
-            ItemEntity drop = new ItemEntity(level, vec3.x(), vec3.y(), vec3.z(), earthwormFarm);
+            ItemStack earthwormDirt = new ItemStack(BlocksRegistry.WORMY_DIRT.get());
+            ItemEntity drop = new ItemEntity(level, vec3.x(), vec3.y(), vec3.z(), earthwormDirt);
             drop.setDefaultPickUpDelay();
             level.addFreshEntity(drop);
         }
