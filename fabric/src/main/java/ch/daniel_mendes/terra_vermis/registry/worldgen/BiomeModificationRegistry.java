4f6d3ee691841bf331f4fd5981c69ee5df757579
Daniel Mendes <contact@daniel-mendes.ch>
2025-05-21 23:02:27 +0200

    refactor: remove unused files

--- fabric/src/main/java/ch/daniel_mendes/terra_vermis/registry/worldgen/BiomeModificationRegistry.java
+++ fabric/src/main/java/ch/daniel_mendes/terra_vermis/registry/worldgen/BiomeModificationRegistry.java
@@ -1,15 +1,23 @@
 package ch.daniel_mendes.terra_vermis.registry.worldgen;
 
+import ch.daniel_mendes.terra_vermis.Constants;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class BiomeModificationRegistry {
+    public static ResourceKey<PlacedFeature> createPlacedFeatureKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, Constants.id(name));
+    }
+
     public static void register() {
         BiomeModifications.addFeature(
             BiomeSelectors.foundInOverworld(),
             GenerationStep.Decoration.VEGETAL_DECORATION,
-            PlacedFeatureRegistry.createPlacedFeatureKey("disk_earthworm")
+            createPlacedFeatureKey("disk_earthworm")
         );
     }
 }
