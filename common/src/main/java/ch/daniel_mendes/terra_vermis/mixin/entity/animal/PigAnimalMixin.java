7dba34f9424f8922635d1bb9978803b753efd483
Daniel Mendes <contact@daniel-mendes.ch>
2025-05-25 22:03:09 +0200

    fix: Fixed can't feed pig

--- common/src/main/java/ch/daniel_mendes/terra_vermis/mixin/entity/animal/PigAnimalMixin.java
+++ common/src/main/java/ch/daniel_mendes/terra_vermis/mixin/entity/animal/PigAnimalMixin.java
@@ -2,15 +2,14 @@ package ch.daniel_mendes.terra_vermis.mixin.entity.animal;
 
 import ch.daniel_mendes.terra_vermis.entity.ai.goal.EatEarthwormBlockGoal;
 import ch.daniel_mendes.terra_vermis.entity.animal.ChickenAccessor;
-import net.minecraft.core.particles.BlockParticleOption;
-import net.minecraft.core.particles.ParticleTypes;
+import ch.daniel_mendes.terra_vermis.mixin.entity.animal.util.EarthwormEatingLogic;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
@@ -19,23 +18,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Chicken.class)
-public class ChickenAnimalMixin extends Animal implements ChickenAccessor {
+@Mixin(Pig.class)
+public class PigAnimalMixin extends Animal implements ChickenAccessor {
     private int eatAnimationTick;
     private EatEarthwormBlockGoal eatEarthwormBlockGoal;
 
-    protected ChickenAnimalMixin(EntityType<? extends Animal> entityType, Level level) {
+    protected PigAnimalMixin(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
     }
 
     @Override
     public boolean isFood(ItemStack stack) {
-        return stack.is(ItemTags.CHICKEN_FOOD);
+        return stack.is(ItemTags.PIG_FOOD);
     }
 
     @Override
     public @Nullable AgeableMob getBreedOffspring(ServerLevel level, AgeableMob otherParent) {
-        return null;
+       return null;
     }
 
     @Inject(method = "registerGoals", at = @At("TAIL"))
@@ -50,8 +49,8 @@ public class ChickenAnimalMixin extends Animal implements ChickenAccessor {
         super.customServerAiStep(level);
     }
 
-    @Inject(method = "aiStep", at = @At("TAIL"))
-    private void onAiStep(CallbackInfo ci) {
+    @Override
+    public void aiStep() {
         if (this.level().isClientSide) {
             this.eatAnimationTick = Math.max(0, this.eatAnimationTick - 1);
         }
@@ -61,23 +60,7 @@ public class ChickenAnimalMixin extends Animal implements ChickenAccessor {
     public void handleEntityEvent(byte id) {
         if (id == EatEarthwormBlockGoal.EAT_ANIMATION_EVENT) {
             this.eatAnimationTick = EatEarthwormBlockGoal.EAT_ANIMATION_TICKS + this.random.nextInt(20);
-
-            if (this.level().isClientSide) {
-                BlockParticleOption particle = new BlockParticleOption(
-                        ParticleTypes.BLOCK, this.getBlockStateOn());
-
-                // Add particles on the ground
-                for (int i = 0; i < 5; i++) {
-                    double x = this.getX() + this.random.nextGaussian() * 0.2;
-                    double y = this.getY() + this.random.nextGaussian() * 0.2;
-                    double z = this.getZ() + this.random.nextGaussian() * 0.2;
-                   this.level().addParticle(particle,
-                            x, y, z,
-                            this.random.nextGaussian() * 0.1,
-                            this.random.nextGaussian() * 0.1,
-                            this.random.nextGaussian() * 0.1);
-                }
-            }
+            EarthwormEatingLogic.handleEatParticle(this.level(), this, this.random);
         } else {
             super.handleEntityEvent(id);
         }
